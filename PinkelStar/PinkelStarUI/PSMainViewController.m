7575aa69cd97b62666f7b80e2e3fe6a258150a4d
Alexander van Elsas <alexander.vanelsas@gmail.com>
2011-05-17 16:39:17 +0200

    Fixed an issue that caused email not to work properly. Upgraded the Framework to the latest iOS SDK 4.3.2

--- PinkelStar/PinkelStarUI/PSMainViewController.m
+++ PinkelStar/PinkelStarUI/PSMainViewController.m
@@ -103,7 +103,8 @@ static CGFloat permissionViewOffsetY = 26.0;
 	//return @"v.0.9.3";
 	//return @"v.0.9.4";
 	//return @"v.0.9.5";
-	return @"v.0.9.6";	
+	//return @"v.0.9.6";	
+	return @"v.0.9.7";	
 }
 
 
